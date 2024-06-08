bfb95c36a60e2702ef790ebc043fea871ca9964f
Siracenco Serghei <siracencoserghei@email.com>
2024-06-08 15:21:26 +0200

    day_11

---
+++ day_11/blackjack_dict.py
@@ -0,0 +1,9 @@
+
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+emoji_suits = {
+            'Hearts': '♥',
+            'Diamonds': '♦',
+            'Clubs': '♣',
+            'Spades': '♠'
+        }
---
+++ day_11/clear.py
@@ -0,0 +1,7 @@
+
+import os
+def clear():
+    if 'TERM' in os.environ:
+        os.system('clear' if os.name != 'nt' else 'cls')
+    else:
+        print("\033[H\033[J", end="")
