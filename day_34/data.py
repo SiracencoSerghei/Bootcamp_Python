97e6a2c0d1f80c88f96894479d76086f4167f889
Siracenco Serghei <siracencoserghei@email.com>
2024-06-29 20:35:53 +0200

    day 34

--- day_34/data.py
+++ day_34/data.py
@@ -6,8 +6,8 @@ question_data = [
         "question": "The HTML5 standard was published in 2014.",
         "correct_answer": "True",
         "incorrect_answers": [
-            "False"
-        ]
+            "False",
+        ],
     },
     {
         "category": "Science: Computers",
@@ -16,8 +16,8 @@ question_data = [
         "question": "The first computer bug was formed by faulty wires.",
         "correct_answer": "False",
         "incorrect_answers": [
-            "True"
-        ]
+            "True",
+        ],
     },
     {
         "category": "Science: Computers",
@@ -26,8 +26,8 @@ question_data = [
         "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
         "correct_answer": "False",
         "incorrect_answers": [
-            "True"
-        ]
+            "True",
+        ],
     },
     {
         "category": "Science: Computers",
@@ -36,8 +36,8 @@ question_data = [
         "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
         "correct_answer": "False",
         "incorrect_answers": [
-            "True"
-        ]
+            "True",
+        ],
     },
     {
         "category": "Science: Computers",
@@ -46,8 +46,8 @@ question_data = [
         "question": "Linus Torvalds created Linux and Git.",
         "correct_answer": "True",
         "incorrect_answers": [
-            "False"
-        ]
+            "False",
+        ],
     },
     {
         "category": "Science: Computers",
@@ -56,8 +56,8 @@ question_data = [
         "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
         "correct_answer": "False",
         "incorrect_answers": [
-            "True"
-        ]
+            "True",
+        ],
     },
     {
         "category": "Science: Computers",
@@ -66,8 +66,8 @@ question_data = [
         "question": "AMD created the first consumer 64-bit processor.",
         "correct_answer": "True",
         "incorrect_answers": [
-            "False"
-        ]
+            "False",
+        ],
     },
     {
         "category": "Science: Computers",
@@ -76,8 +76,8 @@ question_data = [
         "question": "'HTML' stands for Hypertext Markup Language.",
         "correct_answer": "True",
         "incorrect_answers": [
-            "False"
-        ]
+            "False",
+        ],
     },
     {
         "category": "Science: Computers",
@@ -86,8 +86,8 @@ question_data = [
         "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
         "correct_answer": "True",
         "incorrect_answers": [
-            "False"
-        ]
+            "False",
+        ],
     },
     {
         "category": "Science: Computers",
@@ -96,7 +96,7 @@ question_data = [
         "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
         "correct_answer": "False",
         "incorrect_answers": [
-            "True"
-        ]
-    }
+            "True",
+        ],
+    },
 ]
