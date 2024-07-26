295ccd84303fbad965e50fe4de01daea9cd585f4
Siracenco Serghei <siracencoserghei@email.com>
2024-07-26 13:39:58 +0200

    day 64 form

--- day_64/main.py
+++ day_64/main.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float, DATE
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 import requests
 import os
 import dotenv
@@ -38,6 +38,15 @@ class Movie(db.Model):
     review: Mapped[str] = mapped_column(String(250), nullable=True)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
     
+class EditForm(FlaskForm):
+    rating = StringField(label='Your Rating out of 10 e.g. 7.5', validators=[DataRequired(), ])
+    review = StringField(label='Your Review', validators=[DataRequired(), Length(min=1, max=75)])
+    submit = SubmitField(label='Done')
+    
+class FindMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
+    
 with app.app_context():
     db.create_all()
     
@@ -50,13 +59,56 @@ with app.app_context():
 #     review="My favourite character was the caller.",
 #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
 # )
+#     second_movie = Movie(
+#         title="Avatar The Way of Water",
+#         year=2022,
+#         description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#         rating=7.3,
+#         ranking=9,
+#         review="I liked the water.",
+#         img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+#     )
 # with app.app_context():
 #     db.session.add(new_movie)
 #     db.session.commit()
 
+def get_all_movies():
+    with app.app_context():
+        result = db.session.execute(db.select(Movie).order_by(Movie.ranking))
+        all_movies = result.scalars().all()
+        return all_movies
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", movies=get_all_movies())
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    form = FindMovieForm()
+    return render_template("add.html", form = form)
+
+@app.route('/edit',methods=["GET", "POST"])
+def edit():
+    form = EditForm()
+    movie_id = request.args.get('id')
+    movie = db.get_or_404(Movie,movie_id)
+    if form.validate_on_submit():
+        try:
+            movie.rating = float(form.rating.data)
+        except ValueError:
+            movie.rating = movie.rating
+        finally:
+            movie.review = form.review.data
+            db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit.html',movie=movie,form=form)
+
+@app.route("/delete")
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
