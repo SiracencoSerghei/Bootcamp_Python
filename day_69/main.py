8d21f17c4468e968a5e8dfe6790f2c9f0200668b
Siracenco Serghei <siracencoserghei@email.com>
2024-08-01 13:40:18 +0200

    day 69

--- day_69/main.py
+++ day_69/main.py
@@ -12,20 +12,24 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-import os
-import dotenv
 
-from forms import CreatePostForm, RegisterForm, LoginForm
+'''
+Make sure the required packages are installed: 
+Open the Terminal in PyCharm (bottom left). 
 
-dotenv.load_dotenv()
+On Windows type:
+python -m pip install -r requirements.txt
 
-SECRET = os.environ.get("PASSWD")
-basedir = os.path.abspath(os.path.dirname(__file__))
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from the requirements.txt for this project.
+'''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET
-Bootstrap5(app)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
+Bootstrap5(app)
 
 # Configure Flask-Login
 login_manager = LoginManager()
@@ -47,38 +51,29 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 
-
-
-
 # CREATE DATABASE
 class Base(DeclarativeBase):
-	pass
-
-
-db_path = os.path.join(basedir, "instance/posts.db")
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
-# CONFIGURE TABLE
+# CONFIGURE TABLES
 class BlogPost(db.Model):
-	__tablename__ = "blog_posts"
-	id: Mapped[int] = mapped_column(Integer, primary_key=True)
-	# Create Foreign Key, "users.id" the users refers to the tablename of User.
-	author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
-	# Create reference to the User object. The "posts" refers to the posts property in the User class.
-	author = relationship("User", back_populates="posts")
-	title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-	subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
-	date: Mapped[str] = mapped_column(String(250), nullable=False)
-	body: Mapped[str] = mapped_column(Text, nullable=False)
-	img_url: Mapped[str] = mapped_column(String(250), nullable=False)
-	# Parent relationship to the comments
-	comments = relationship("Comment", back_populates="parent_post")
-	
-	def to_dict(self):
-		return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+    __tablename__ = "blog_posts"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    # Create Foreign Key, "users.id" the users refers to the tablename of User.
+    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    # Create reference to the User object. The "posts" refers to the posts property in the User class.
+    author = relationship("User", back_populates="posts")
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    # Parent relationship to the comments
+    comments = relationship("Comment", back_populates="parent_post")
 
 
 # Create a User table for all your registered users
@@ -93,7 +88,8 @@ class User(UserMixin, db.Model):
     posts = relationship("BlogPost", back_populates="author")
     # Parent relationship: "comment_author" refers to the comment_author property in the Comment class.
     comments = relationship("Comment", back_populates="comment_author")
-    
+
+
 # Create a table for the comments on the blog posts
 class Comment(db.Model):
     __tablename__ = "comments"
@@ -109,7 +105,8 @@ class Comment(db.Model):
 
 
 with app.app_context():
-	db.create_all()
+    db.create_all()
+
 
 # Create an admin-only decorator
 def admin_only(f):
@@ -131,7 +128,7 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts, current_user=current_user)
 
 
-# @ Add a POST method to be able to post comments
+# Add a POST method to be able to post comments
 @app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
@@ -214,6 +211,7 @@ def about():
 def contact():
     return render_template("contact.html", current_user=current_user)
 
+
 # Register new users into the User database
 @app.route('/register', methods=["GET", "POST"])
 def register():
@@ -273,7 +271,5 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('get_all_posts'))
-
-
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=True, port=5001)
